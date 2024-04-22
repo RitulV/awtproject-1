02f0389988b746c925847d03f6239bd9d0800184
MT <meetprakash342004@gmail.com>
2024-04-22 22:57:09 +0530

    final

--- server.js
+++ server.js
@@ -12,22 +12,18 @@ connectDB();
 const transactions = require('./routes/transactions');
 
 const app = express();
+var cors = require('cors')
+
+app.use(cors());
 
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
 
 app.use('/api/v1/transactions', transactions);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(PORT, console.log(`Server running in  port ${PORT}`.yellow.bold));
 
